3d3246872653987cac4a466e68792599080c4262
Wayne <wayne5540.au+fork@gmail.com>
2023-06-03 22:40:15 -0700

    Add GA

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,18 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
-
 import sitemap from '@astrojs/sitemap';
+
+import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://ailagi.com',
-	integrations: [mdx(), sitemap()],
+  site: 'https://ailagi.com',
+	integrations: [
+		mdx(),
+		sitemap(),
+		partytown({
+			config: { 
+				forward: ["dataLayer.push"] 
+			},
+		})]
 });
